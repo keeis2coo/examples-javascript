343e49073f245a0d3304f6e6c0b8db220896d9f3
keeis2coo <kjarr88@gmail.com>
2016-01-15 15:19:02 -0600

    Create squared-loop.js
    basic loop to display square root from 1-30

---
+++ squared-loop.js
@@ -0,0 +1,12 @@
+<script>
+				
+			for (var i=1; i < 31; i++)
+				
+				{	
+				var root= Math.pow(i,2)				
+				document.write(i +" squared= " + root + "<br>");
+				}
+				
+				
+		
+		</script>
